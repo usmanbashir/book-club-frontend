bd9a683e6b79f586207adeabbfcde9b4501f4aee
Catherine Nevin <cat_nevin@hotmail.com>
2023-05-31 11:50:55 +0100

    First commit. Create SignUp and LogIn components. Add forms.

---
+++ src/components/LogIn.js
@@ -0,0 +1,24 @@
+
+
+export default function Login() {
+
+return (
+    <div>
+        <h2>Log in form</h2>
+
+        <form>
+            <div>
+                <label>Username</label>
+                <input></input>
+            </div>
+
+            <div>
+                <label>Password</label>
+                <input></input>
+            </div>
+        </form>
+    </div>
+
+)
+
+}
---
+++ src/components/SignUp.js
@@ -0,0 +1,35 @@
+
+
+export default function SignUp() {
+
+return(
+
+    <div>
+        <h2>Sign up form</h2>
+
+        <form>
+            <div>
+                <label>Username</label>
+                <input></input>
+            </div>
+
+            <div>
+                <label>First Name</label>
+                <input></input>
+            </div>
+
+            <div>
+                <label>Last Name</label>
+                <input></input>
+            </div>
+
+            <div>
+                <label>Password</label>
+                <input></input>
+            </div>
+        </form>
+    </div>
+    
+)
+
+}
