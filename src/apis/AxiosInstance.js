c60f9a34c414586d17f43b3762f39dddfea70fb9
Catherine Nevin <cat_nevin@hotmail.com>
2023-07-25 16:05:14 +0100

    Add Heroku backend URL.

--- src/apis/AxiosInstance.js
+++ src/apis/AxiosInstance.js
@@ -4,7 +4,7 @@ import axios from 'axios';
 
 const axiosInstance = axios.create({
     baseURL: process.env.NODE_ENV === "production"
-        ? "https:/placeholder.com/"        
+        ? "https://secure-gorge-86146-38ded5b1c080.herokuapp.com/"        
         : "http://localhost:4000",
     headers: {
         'Content-Type': 'application/json'
